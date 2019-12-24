c83a8cd768019b494bd27337ca93aa2ec168b300
stepanbog <step300699@yandex.ru>
2019-12-24 16:39:07 +0300

    Scripted auto-commit on change (2019-12-24 16:39:07) by gitwatch.sh

--- Lab7/src/main/java/Client.java
+++ Lab7/src/main/java/Client.java
@@ -1,15 +1,25 @@
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMQException;
+
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args){
         System.out.println("connecting");
-        ZContext context = new ZContext();
-        ZMQ.Socket socket = new context.createSocket(SocketType.REP);
-        socket.connect("tcp://localhost:5555");
+        try {
 
 
+            ZContext context = new ZContext();
+            ZMQ.Socket socket = new context.createSocket(SocketType.REP);
+            socket.connect("tcp://localhost:5555");
+            Scanner in = new Scanner(System.in);
+            String mes = in.nextLine();
+            MesToServer mesToServ =
+        } catch (ZMQException ex){
+            ex.printStackTrace();
+        }
     }
 
 }
