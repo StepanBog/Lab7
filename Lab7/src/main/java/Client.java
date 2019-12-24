31d00c442da6dfd1337a66f07feb1139695e3ca1
stepanbog <step300699@yandex.ru>
2019-12-24 16:27:48 +0300

    Scripted auto-commit on change (2019-12-24 16:27:48) by gitwatch.sh

--- Lab7/src/main/java/Client.java
+++ Lab7/src/main/java/Client.java
@@ -1,9 +1,11 @@
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
 public class Client {
     public static void main(String[] args){
+        System.out.println("connecting");
         ZContext connect = new ZContext();
-        
+        ZMQ.context()
 
     }
 
