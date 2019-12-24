41fb6258b6e0d4677c10d5aef890bc34d8dde8e1
stepanbog <step300699@yandex.ru>
2019-12-24 16:47:40 +0300

    Scripted auto-commit on change (2019-12-24 16:47:40) by gitwatch.sh

--- Lab7/src/main/java/Client.java
+++ Lab7/src/main/java/Client.java
@@ -6,8 +6,6 @@ public class Client {
     public static void main(String[] args){
         System.out.println("connecting");
         try {
-
-
             ZContext context = new ZContext();
             ZMQ.Socket socket = new context.createSocket(SocketType.REP);
             socket.connect("tcp://localhost:5555");
@@ -17,7 +15,8 @@ public class Client {
             ZMsg zmesAns = new ZMsg();
             zmesSend.add(mes);
             zmesSend.send(socket);
-            zmesAns.
+            ZMsg.recvMsg(socket);
+            System.out.print("IN:" + zmesAns.popString());
         } catch (ZMQException ex){
             ex.printStackTrace();
         }
