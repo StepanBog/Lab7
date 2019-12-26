13bdd3e6c3c297c303b8627b5448c865a7ea4db3
stepanbog <step300699@yandex.ru>
2019-12-26 12:47:06 +0300

    Scripted auto-commit on change (2019-12-26 12:47:06) by gitwatch.sh

--- Lab7/src/main/java/Server.java
+++ Lab7/src/main/java/Server.java
@@ -1,9 +1,16 @@
 import org.zeromq.ZContext;
 
 public class Server {
-    private ZContext
+    private ZContext zcon;
+
+    public Server(ZContext zcon) {
+        this.zcon = zcon;
+        this.fro
+    }
+
     public static void main(String[] args){
         ZContext zcon = new ZContext();
+        Server server = new Server(zcon);
     }
 
 }
