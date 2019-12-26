a3c838e0c4e69b24cebeddf0c53ace0ab48ca7c0
stepanbog <step300699@yandex.ru>
2019-12-26 13:36:26 +0300

    Scripted auto-commit on change (2019-12-26 13:36:26) by gitwatch.sh

--- Lab7/src/main/java/Commutator.java
+++ Lab7/src/main/java/Commutator.java
@@ -1,12 +1,30 @@
 public class Commutator {
     private String leftBound;
     private String rightBound;
+    private long time;
 
+    Commutator(String leftBound, String rightBound, long time){
+        this.time = time;
+        this.leftBound = leftBound;
+        this.rightBound = rightBound;
+    }
     public String getLeftBound() {
         return leftBound;
     }
 
     public String getRightBound() {
         return rightBound;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public boolean intersect(String value){
+        return Integer.parseInt(leftBound) <=
     }
 }
