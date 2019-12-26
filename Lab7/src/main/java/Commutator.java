e60e66452b374b759270617f5ec94bcbccf7b74b
stepanbog <step300699@yandex.ru>
2019-12-26 13:33:46 +0300

    Scripted auto-commit on change (2019-12-26 13:33:46) by gitwatch.sh

--- Lab7/src/main/java/Commutator.java
+++ Lab7/src/main/java/Commutator.java
@@ -1,2 +1,12 @@
 public class Commutator {
+    private String leftBound;
+    private String rightBound;
+
+    public String getLeftBound() {
+        return leftBound;
+    }
+
+    public String getRightBound() {
+        return rightBound;
+    }
 }
