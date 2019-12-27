0f62fb4a2863af0031539601e1889ea21545d88f
stepanbog <step300699@yandex.ru>
2019-12-27 17:59:07 +0300

    Scripted auto-commit on change (2019-12-27 17:59:07) by gitwatch.sh

--- Lab7/src/main/java/Cache.java
+++ Lab7/src/main/java/Cache.java
@@ -1,2 +1,10 @@
+import org.zeromq.ZContext;
+
 public class Cache {
+    public static void main(String args[]){
+        try{
+            ZContext zcon = new ZContext();
+            
+        }
+    }
 }
