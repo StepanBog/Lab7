96f14894636faeb66f34d3e2f105acc292c767c8
stepanbog <step300699@yandex.ru>
2019-12-27 18:03:36 +0300

    Scripted auto-commit on change (2019-12-27 18:03:36) by gitwatch.sh

--- Lab7/src/main/java/Cache.java
+++ Lab7/src/main/java/Cache.java
@@ -1,10 +1,30 @@
 import org.zeromq.ZContext;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Cache {
+    private final ZContext zcon;
+    private int leftBound;
+    private int rightBound;
+    private HashMap<Integer,String> cache;
+    
+    public Cache(ZContext zcon) {
+        this.zcon = zcon;
+        cache = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        leftBound = in.nextInt();
+        rightBound = in.nextInt();
+
+    }
+
     public static void main(String args[]){
         try{
             ZContext zcon = new ZContext();
-            
+            Cache cache = new Cache(zcon);
+
+        } catch (Exception e){
+            System.out.print(e.toString());
         }
     }
 }
