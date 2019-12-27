92e369822c84ac5af3c0b4e06e8e0c6d3bc725be
stepanbog <step300699@yandex.ru>
2019-12-27 20:09:07 +0300

    Scripted auto-commit on change (2019-12-27 20:09:07) by gitwatch.sh

--- Lab7/src/main/java/Proxy.java
+++ Lab7/src/main/java/Proxy.java
@@ -44,7 +44,7 @@ public class Proxy{
             }
             if (items.pollin(1)){
                 ZMsg msg = ZMsg.recvMsg(backend);
-                System.out.println("Have msg from cache");
+          //      System.out.println("Have msg from cache");
                 if (msg != null)
                     handleDealerPollin(msg);
                 else
@@ -60,7 +60,7 @@ public class Proxy{
     private void handleDealerPollin(ZMsg msg) {
         String[] data = msg.getLast().toString().split(SPACE);
         if (msg.getLast().toString().contains("I_AM_ALIVE")) {
-            System.out.println("Have Alive msg ");
+      //      System.out.println("Have Alive msg ");
             if (!commutator.containsKey(msg.getFirst())) {
                 Commutator com = new Commutator(data[1], data[2], System.currentTimeMillis());
                 commutator.put(msg.getFirst().duplicate(), com);
