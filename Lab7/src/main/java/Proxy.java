046ca555de7158abd0fb240a54ab27dad959575b
stepanbog <step300699@yandex.ru>
2019-12-27 18:52:29 +0300

    Scripted auto-commit on change (2019-12-27 18:52:29) by gitwatch.sh

--- Lab7/src/main/java/Proxy.java
+++ Lab7/src/main/java/Proxy.java
@@ -89,6 +89,7 @@ public class Proxy{
         for (HashMap.Entry<ZFrame,Commutator> c: commutator.entrySet()) {
             if (c.getValue().intersect(data[1])){
                 ZMsg mes = msg.duplicate();
+                mes.send()
             }
         }
     }
