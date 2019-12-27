7e508abf22febb956eab99e3dda07cd03973a2fd
stepanbog <step300699@yandex.ru>
2019-12-27 18:50:42 +0300

    Scripted auto-commit on change (2019-12-27 18:50:42) by gitwatch.sh

--- Lab7/src/main/java/Proxy.java
+++ Lab7/src/main/java/Proxy.java
@@ -86,7 +86,7 @@ public class Proxy{
     }
 
     private void recievePUT(String[] data, ZMsg msg) {
-        for (:
+        for (Commutator i: commutator.entrySet())
              ) {
             
         }
