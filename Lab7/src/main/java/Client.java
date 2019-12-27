ea273c1845b8c1d6c85ed94ad5296425b8d49558
stepanbog <step300699@yandex.ru>
2019-12-27 20:11:48 +0300

    Scripted auto-commit on change (2019-12-27 20:11:48) by gitwatch.sh

--- Lab7/src/main/java/Client.java
+++ Lab7/src/main/java/Client.java
@@ -19,7 +19,9 @@ public class Client {
                 if (mes.contains("PUT") || mes.contains("GET")) {
                     zmesSend.add(mes);
                     zmesSend.send(socket);
+
                     ZMsg.recvMsg(socket);
+                    if ()
                     System.out.print("IN:" + zmesAns.popString());
                 } else {
                     System.out.println("error");
