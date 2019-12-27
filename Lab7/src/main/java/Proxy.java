bbdd2451ab7683a930e2ea177d9083eb987280e1
stepanbog <step300699@yandex.ru>
2019-12-27 18:35:22 +0300

    Scripted auto-commit on change (2019-12-27 18:35:22) by gitwatch.sh

--- Lab7/src/main/java/Proxy.java
+++ Lab7/src/main/java/Proxy.java
@@ -52,7 +52,7 @@ public class Proxy{
                 commutator.get(msg.getFirst().duplicate()).setTime(System.currentTimeMillis());
             }
         } else {
-            
+            msg.poll()
         }
     }
 
